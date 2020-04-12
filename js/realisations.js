4214fd0b3e2dd58057c1003af627affef22bb28c
Stéphane Chaffre <erkomxtil@gmail.com>
2020-04-12 15:38:12 +0200

    mise en place des données dans la page réalisations et ajustement du json pour lvmh fenty

--- js/realisations.js
+++ js/realisations.js
@@ -1,13 +1,10 @@
 import { ajaxGet } from './ajax.js'
 
 function realisations () {
-  let main = document.getElementById("main")
   let cvLink = document.getElementById("realisations")
   
   cvLink.addEventListener("click", (e) => {
-    main.innerHTML = ""
-    main.className = "realisations"
-    main.innerHTML = "<h1>Mes réalisations</h1>"
+    affichagePageRealisations () 
   })
 }
 
@@ -16,12 +13,42 @@ function affichagePageRealisations () {
   let urlSite = document.location.href
   let newUrlSite = urlSite.split("1:")
   let finalUrlSite = newUrlSite[1].substr(0,5)
-  console.log("hello")
+
   ajaxGet("http://127.0.0.1:"+ finalUrlSite +"/data.json", (reponse) => {
     let dataInfos = JSON.parse(reponse)
+    let main = document.getElementById("main")
+    let realisationsInfos = dataInfos.realisations
+    main.innerHTML = ""
+    main.className = "realisations"
+    main.innerHTML = "<h1>Mes réalisations</h1>"
 
-    console.log(dataInfos.realisations)
-  
+    for (let i = 0; i < realisationsInfos.length ; i++){
+      let wrapper = document.createElement("div")
+      let imagesBlock = document.createElement("img")
+      wrapper.className = "realisationsBlock"
+      imagesBlock.src = realisationsInfos[i].logo
+      imagesBlock.alt = realisationsInfos[i].entreprise
+      imagesBlock.dataset.numeroLogo = i
+
+      imagesBlock.addEventListener("click", (e) => {
+        e.preventDefault()
+        main.innerHTML = ""
+        let numeroLogo = e.currentTarget.dataset.numeroLogo
+        let date = realisationsInfos[numeroLogo].date
+        let entreprise = realisationsInfos[numeroLogo].entreprise
+        let fonction = realisationsInfos[numeroLogo].fonction
+        let environement = realisationsInfos[numeroLogo].environement
+        let description = realisationsInfos[numeroLogo].description
+        let logo = realisationsInfos[numeroLogo].logo
+
+        let paragraphePhoto = document.createElement("p")
+        paragraphePhoto.innerHTML = '<img src="'+ logo +'" alt="'+ entreprise + '"><br>'+ date + '<br>Entreprise : ' + entreprise + '<br>Fonction : ' + fonction + '<br>Environement : ' + environement + '<br>Description : ' + description
+
+        main.appendChild(paragraphePhoto)
+      })
+      main.appendChild(wrapper)
+      main.appendChild(imagesBlock)
+    }  
   })
 }
 
