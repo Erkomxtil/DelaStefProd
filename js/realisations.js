ee957f0ddc23dc209d54c2ed912dc0082150c898
Stéphane Chaffre <erkomxtil@gmail.com>
2020-04-17 22:27:21 +0200

    ajout du click pour afficher la fiche complète plus retour sur la page logo réalisations

--- js/realisations.js
+++ js/realisations.js
@@ -18,11 +18,13 @@ function affichagePageRealisations () {
     let dataInfos = JSON.parse(reponse)
 
     /* Titre pour la partie mobile */
-    let headerTitle = document.getElementById("titreNavigation")
-    let titleH1 = document.createElement("h1")
-    headerTitle.innerHTML = ""
-    titleH1.textContent = "réalisations"
-    headerTitle.appendChild(titleH1)
+    function titreMobile () {
+      let headerTitle = document.getElementById("titreNavigation")
+      let titleH1 = document.createElement("h1")
+      headerTitle.innerHTML = ""
+      titleH1.textContent = "réalisations"
+      headerTitle.appendChild(titleH1)
+    }
 
     /* Le contenu de la partie main */
     let main = document.getElementById("main")
@@ -31,34 +33,71 @@ function affichagePageRealisations () {
     main.innerHTML = ""
     main.className = "realisations"
 
-    for (let i = 0; i < realisationsInfos.length ; i++){
-      let wrapper = document.createElement("div")
+    /* affichage des logos sur la page */
+    function blockImg () {
       let imagesBlock = document.createElement("img")
-      wrapper.className = "realisationsBlock"
-      imagesBlock.className = "imgLink"
-      imagesBlock.src = realisationsInfos[i].logo
-      imagesBlock.alt = realisationsInfos[i].entreprise
-      imagesBlock.dataset.numeroLogo = i
+      let wrapper = document.createElement("div")
 
-      imagesBlock.addEventListener("click", (e) => {
-        e.preventDefault()
-        main.innerHTML = ""
-        let numeroLogo = e.currentTarget.dataset.numeroLogo
-        let date = realisationsInfos[numeroLogo].date
-        let entreprise = realisationsInfos[numeroLogo].entreprise
-        let fonction = realisationsInfos[numeroLogo].fonction
-        let environement = realisationsInfos[numeroLogo].environement
-        let description = realisationsInfos[numeroLogo].description
-        let logo = realisationsInfos[numeroLogo].logo
+      for (let i = 0; i < realisationsInfos.length ; i++){
+        wrapper.className = "realisationsBlock"
+        imagesBlock.className = "imgLink"
+        imagesBlock.src = realisationsInfos[i].logo
+        imagesBlock.alt = realisationsInfos[i].entreprise
+        imagesBlock.dataset.numeroLogo = i
+              
+        wrapper.appendChild(imagesBlock)
+        main.appendChild(wrapper)
+      }
+      main.innerHTML = ""
+      imagesBlock.addEventListener("click", infosBlock())
+    }
 
-        let paragraphePhoto = document.createElement("p")
-        paragraphePhoto.innerHTML = '<img class="imgLink" src="'+ logo +'" alt="'+ entreprise + '"><br>'+ date + '<br>Entreprise : ' + entreprise + '<br>Fonction : ' + fonction + '<br>Environement : ' + environement + '<br>Description : ' + description
+    /* affichage complet logo plus informations */
+    function infosBlock () {
+      for (let i = 0; i < realisationsInfos.length ; i++){
+        let wrapper = document.createElement("div")
+        let imagesBlock = document.createElement("img")
 
-        main.appendChild(paragraphePhoto)
-      })
-      wrapper.appendChild(imagesBlock)
-      main.appendChild(wrapper)
-    }  
+        wrapper.className = "realisationsBlock"
+        imagesBlock.className = "imgLink"
+        imagesBlock.src = realisationsInfos[i].logo
+        imagesBlock.alt = realisationsInfos[i].entreprise
+        imagesBlock.dataset.numeroLogo = i
+              
+        wrapper.appendChild(imagesBlock)
+        main.appendChild(wrapper)
+
+        imagesBlock.addEventListener("click", (e) => {
+          main.innerHTML = ""
+          let numeroLogo = e.currentTarget.dataset.numeroLogo
+          let date = realisationsInfos[numeroLogo].date
+          let entreprise = realisationsInfos[numeroLogo].entreprise
+          let fonction = realisationsInfos[numeroLogo].fonction
+          let environement = realisationsInfos[numeroLogo].environement
+          let description = realisationsInfos[numeroLogo].description
+          let logo = realisationsInfos[numeroLogo].logo
+    
+          let paragraphePhoto = document.createElement("p")
+          let closeInfos = document.createElement("span")
+          closeInfos.id = "closeInfos"
+          closeInfos.textContent = "X"
+          paragraphePhoto.id = "paragrapheInfos"
+          paragraphePhoto.innerHTML = '<img class="imgLink" src="'+ logo +'" alt="'+ entreprise + '"><br>'+ date + '<br>Entreprise : ' + entreprise + '<br>Fonction : ' + fonction + '<br>Environement : ' + environement + '<br>Description : ' + description
+    
+          paragraphePhoto.appendChild(closeInfos)
+          main.appendChild(paragraphePhoto)
+          
+          closeInfos.addEventListener("click", (e) => {
+            main.innerHTML = ""
+            blockImg()
+          })
+        })
+      }
+    }
+
+    titreMobile()
+
+    infosBlock()
   })
 }
 
